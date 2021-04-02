6198d7fcbdc4bd73ba3a4093d6e7a8da52c725eb
Nickolay <mr-nicko89@yandex.ru>
2021-04-02 22:44:25 +0300

    На основе класса Timer создал бин. Связал с AnimalCage. Проверил, что при выполнении метода main время, которое пишет таймер, одно и тоже.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -10,12 +10,14 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
